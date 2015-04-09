bf9b23a8bb0fe6f36f6815c53ac78b079a0f564d
Jorrit den Broeder <jorrit@parksound.nl>
2015-04-09 14:13:50 +0200

    Revert "fix(license upload): delete back image altogether, not required anymore [MW-1241]"
    This reverts commit 35c3abe62e38f157dcb671115e1321a626112253.

--- src/app/person/license/PersonLicenseController.js
+++ src/app/person/license/PersonLicenseController.js
@@ -5,7 +5,8 @@ angular.module('owm.person.license', [])
 .controller('PersonLicenseController', function ($http, $state, authService, personService, alertService, me, $scope) {
 
   var images = {
-    front: null
+    front: null,
+    back : null
   };
 
   $scope.images = images;
@@ -17,17 +18,25 @@ angular.module('owm.person.license', [])
     });
   });
 
+  angular.element('#licenseBackFile').on('change', function (e) {
+    $scope.$apply(function () {
+      images.back = e.target.files[0];
+    });
+  });
+
   $scope.startUpload = function () {
-    if (!images.front) { return; }
+    if (!(images.front || images.back)) { return; }
 
     $scope.isBusy = true;
     alertService.load();
 
+    var multiPartParams = {};
+    if (images.front) { multiPartParams.frontImage = images.front; }
+    if (images.back)  { multiPartParams.backImage  = images.back; }
+
     personService.addLicenseImages({
       person: me.id
-    }, {
-      frontImage: images.front
-    })
+    }, multiPartParams)
     .then(function () {
       alertService.add('success', 'Bedankt voor het uploaden van je rijbewijs', 5000);
       $state.go('owm.person.dashboard');
