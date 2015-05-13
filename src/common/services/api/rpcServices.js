c8bc061c833cc0ae514a4ab2017876b7a39f1f97
Jorrit den Broeder <jorrit@parksound.nl>
2015-05-13 11:40:48 +0200

    rename search to searchV2 (=corresponding api call)

--- src/common/services/api/rpcServices.js
+++ src/common/services/api/rpcServices.js
@@ -50,7 +50,7 @@ angular.module('rpcServices', [])
   this.alter             = m('alter');
   this.select            = m('select');
   this.forOwner          = m('forOwner');
-  this.search            = m('searchV2');
+  this.searchV2          = m('searchV2');
   this.create            = m('create');
   this.getMembers        = m('getMembers');
   this.addMember         = m('addMember');
