155b321ca90e444f1d7b43975e52f49005ec98e4
Bram Tervoort <b.tervoort@gmail.com>
2015-10-14 11:47:38 +0200

    fix MW-1785 do not show redemption option in price Task #MW-1785 - Afkoop eigen risico niet in berekening

--- src/app/resource/components/ReservationForm.js
+++ src/app/resource/components/ReservationForm.js
@@ -194,7 +194,7 @@ angular.module('owm.resource.reservationForm', [])
     if (price.rent > 0) { s += 'Huur: ' + $filter('currency')(price.rent) + '<br/>'; }
     if (price.insurance > 0) { s += 'Verzekering: ' + $filter('currency')(price.insurance) + '<br/>'; }
     if (price.fee > 0) { s += 'Fee: ' + $filter('currency')(price.fee) + '<br/>'; }
-    if (price.redemption > 0) { s+='Afkoop eigen risico: ' + $filter('currency')(price.redemption) + '<br/>'; }
+//    if (price.redemption > 0) { s+='Afkoop eigen risico: ' + $filter('currency')(price.redemption) + '<br/>'; }
     s += 'Totaal: ' + $filter('currency')(price.total);
     return s;
   };
