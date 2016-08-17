e15f38ac8baa2118aed6b880dff0901df0af9ead
Matth96 <Matthias.Dolstra@hva.nl>
2016-08-17 13:49:51 +0200

    redesign thank you pagina van betaling

--- src/common/services/api/rpcServices.js
+++ src/common/services/api/rpcServices.js
@@ -145,6 +145,12 @@ angular.module('rpcServices', [])
   this.get = m('get');
   this.alter = m('alter');
 })
+.service('account2Service', function (api) {
+  var m = function (name) {
+    return api.createRpcMethod('account2.' + name);
+  };
+  this.forMe = m('forMe');
+})
 
 .service('actionService', function (api) {
   var m = function (name) {
