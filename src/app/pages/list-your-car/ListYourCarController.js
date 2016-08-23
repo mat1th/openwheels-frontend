4a409bbec7989f91e9231a6d5fa62cc8dfaf8e53
Matth96 <Matthias.Dolstra@hva.nl>
2016-08-23 11:21:23 +0200

    add first page without styling

--- src/app/pages/list-your-car/ListYourCarController.js
+++ src/app/pages/list-your-car/ListYourCarController.js
@@ -1,7 +1,24 @@
 'use strict';
-angular.module('owm.pages.list-your-car',[])
+angular.module('owm.pages.list-your-car', [])
 
 .controller('ListYourCarController', function ($scope, user) {
+  $scope.licencePlate = {
+    content: '',
+    data: false,
+    showError: false,
+    error: ''
+  };
+  $scope.calculateYourPrice = {
+    total: 265,
+    dayPrice: 25,
+    numberOfDays: 4
+  };
 
-})
-;
+  $scope.$watchCollection(function () {
+    if ($scope.calculateYourPrice) {
+      var sum =  5 * $scope.calculateYourPrice.numberOfDays * 12;
+      $scope.calculateYourPrice.total = $scope.calculateYourPrice.dayPrice + sum;
+    }
+  }, true);
+
+});
