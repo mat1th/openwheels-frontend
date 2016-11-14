8543c8c7ba6dbba29c96662bbf7c5d7e6e6093d5
Freek <freek.boutkan@gmail.com>
2016-11-14 14:58:07 +0100

    test 2

--- src/common/directives/geocoderDirectiveSearchbar.js
+++ src/common/directives/geocoderDirectiveSearchbar.js
@@ -6,7 +6,35 @@ angular.module('geocoderDirective', [])
   return {
     restrict: 'E',
     templateUrl: 'directives/geocoderDirectiveSearchbar.tpl.html',
+    controller: function($scope) {
+      $scope.search = {text: ''};
+    },
     link: function (scope, elm, attrs) {
+
+      var onDetailsCallback = angular.noop;
+
+      scope.placeDetails = null;
+
+      scope.completePlacesOptions = {
+        watchEnter: true,
+        country   : $filter('translateOrDefault')('SEARCH_COUNTRY', 'nl'),
+        types   : 'geocode',
+      };
+
+      scope.$watch('placeDetails', function (newVal, oldVal) {
+        if (!newVal || (newVal === oldVal)) {
+          return;
+        }
+        onDetailsCallback(newVal);
+      });
+
+      elm.attr('ng-autocomplete', '');
+      elm.attr('details', 'placeDetails');
+      elm.attr('options', 'completePlacesOptions');
+
+      $compile(elm)(scope, function (clone) {
+        elm.replaceWith(clone);
+      });
 
     }
   };
