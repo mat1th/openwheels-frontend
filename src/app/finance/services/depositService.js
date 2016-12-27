14514aef3db134b05da503052b0048b3a1211c08
Freek <freek.boutkan@gmail.com>
2016-12-27 15:44:20 +0100

    Signup logo is in branding now

--- src/app/finance/services/depositService.js
+++ src/app/finance/services/depositService.js
@@ -10,6 +10,7 @@ angular.module('owm.finance')
 
     return contractService.requestContract(params)
     .then(function (contractRequest) {
+      console.log(contractRequest);
       $log.debug(contractRequest);
       // if already accept, no need for payment
       if(contractRequest.status === 'accept') {
